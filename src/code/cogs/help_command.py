f4d71c44c07e93ca97a0358471ec0fcd32fbe63f
unknown <knuutila.vilho@gmail.com>
2022-01-07 13:06:38 +0200

    changed help command

--- src/code/cogs/help_command.py
+++ src/code/cogs/help_command.py
@@ -18,7 +18,7 @@ class Help(commands.Cog):
             if help_string.lower() == "help":
                 desc = f"**Bonk Bot** is a fun bot made to bonk people.\nUsing it is simple, just type `bonk @member`"
                 embed = discord.Embed(description = desc)
-                embed.set_footer(text="Profile picture by BunnySimps#0001 • Developer Sivakka#4938")
+                embed.set_footer(text="Pfp by BunnySimps#0001 • Developer Sivakka#4938")
 
                 await message.channel.send(embed=embed)
 
