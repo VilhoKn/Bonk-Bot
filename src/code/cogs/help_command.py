d04e2e3dea6b721fb154f786357cc372f50617a1
Vilho Knuutila <knuutila.vilho@gmail.com>
2022-01-09 00:43:19 +0200

    added leaderboard to help command

--- src/code/cogs/help_command.py
+++ src/code/cogs/help_command.py
@@ -20,7 +20,7 @@ class Help(commands.Cog):
         if split_message[0].lower() == "bonk":
             help_string = split_message[1]
             if help_string.lower() == "help":
-                desc = f"**Bonk Bot** is a fun bot made to bonk people.\nUsing it is simple, just type `bonk @member`"
+                desc = f"**Bonk Bot** is a fun bot made to bonk people.\nUsing it is simple, just type `bonk @member`\nYou can also see the top 5 bonker with\n`bonk leaderboard`"
                 embed = discord.Embed(description = desc)
                 embed.set_footer(text="Pfp by BunnySimps#0001 • Developer Sivakka#4938")
                 await message.channel.send(embed=embed)
